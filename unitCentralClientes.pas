cb3411fa7fb36973225669c69b7fe4dfbca8922e
Thiago Santos <thilego418@gmail.com>
2026-02-16 13:20:20 -0300

    Completando tela principal, iniciando tela de Central de Clientes e iniciando programação de botões da tela principal

---
+++ unitCentralClientes.pas
@@ -0,0 +1,26 @@
+unit unitCentralClientes;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+
+type
+  TformCentralClientes = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  formCentralClientes: TformCentralClientes;
+
+implementation
+
+{$R *.dfm}
+
+end.
--- unitPrincipal.pas
+++ unitPrincipal.pas
@@ -4,10 +4,32 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg, Vcl.Menus;
 
 type
-  TForm1 = class(TForm)
+  TformPrincipal = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    BitBtn3: TBitBtn;
+    MainMenu1: TMainMenu;
+    Cadastros1: TMenuItem;
+    Clientes1: TMenuItem;
+    Vendedores1: TMenuItem;
+    Fornecedores1: TMenuItem;
+    Movimentao1: TMenuItem;
+    VendaPDV1: TMenuItem;
+    Produtos1: TMenuItem;
+    Sistema1: TMenuItem;
+    Configuraes1: TMenuItem;
+    Sair1: TMenuItem;
+    BitBtn4: TBitBtn;
+    BitBtn5: TBitBtn;
+    procedure Sair1Click(Sender: TObject);
+    procedure Clientes1Click(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +37,27 @@ type
   end;
 
 var
-  Form1: TForm1;
+  formPrincipal: TformPrincipal;
 
 implementation
 
 {$R *.dfm}
+
+uses unitCentralClientes;
+
+procedure TformPrincipal.BitBtn1Click(Sender: TObject);
+begin
+  formCentralClientes.ShowModal;
+end;
+
+procedure TformPrincipal.Clientes1Click(Sender: TObject);
+begin
+  formCentralClientes.ShowModal;
+end;
+
+procedure TformPrincipal.Sair1Click(Sender: TObject);
+begin
+  Application.Terminate;
+end;
 
 end.
